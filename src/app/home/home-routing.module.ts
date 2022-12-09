6481b314f537e97fbcacd84197f616682bdd56e0
Alexander Aguirre <keevinaguirre@gmail.com>
2022-12-09 20:31:27 +0100

    Creation of modules and basic components

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full',
+    component: HomeComponent,
   },
 ];
 
@@ -17,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class HomeRoutingModule {}
